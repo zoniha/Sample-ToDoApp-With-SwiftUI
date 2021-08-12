eecde7f9f9d3cdbf3face0b730bd6bad0aacd99a
zoniha <awaawapurin@gmail.com>
2021-08-12 15:23:25 +0900

    Initial Commit

---
+++ ToDoAppWithSwiftUI/ToDoAppWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppWithSwiftUIApp.swift
+//  ToDoAppWithSwiftUI
+//
+//  Created by zoniha on 2021/08/12.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
